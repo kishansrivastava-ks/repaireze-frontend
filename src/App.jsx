e6253d3be00032a74d022d4fd458b8ff4bba61d6
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2024-08-28 22:35:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>hellow world!</div>
+    </>
+  );
+}
+
+export default App;
